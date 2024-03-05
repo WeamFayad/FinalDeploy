2632a122c75c9e004927b2c13a63f0080adf81b0
WeamFayad <150081112+WeamFayad@users.noreply.github.com>
2024-03-06 01:47:39 +0200

    Update mongoDb.configs.js

--- backend/configs/mongoDb.configs.js
+++ backend/configs/mongoDb.configs.js
@@ -2,7 +2,7 @@ const { default: mongoose } = require("mongoose");
 
 // Function to invoke connection to MongoDB
 const connectToMongoDb = () => {
-  mongoose.connect('mongodb://localhost:27017/paws1');
+  mongoose.connect('mongodb://mongo:27017/paws1');
   const connection = mongoose.connection;
   connection.on("error", (error) => {
     console.log("error connecting", error);
